5bf90a68b6015b814539bd174c1cbe9ad4ccbcbd
GodRyosuke <ienvs0828@outlook.jp>
2020-09-08 19:48:29 +0900

    test.jsを修正しました 19:48

--- test.js
+++ test.js
@@ -23,4 +23,21 @@ $('.accordion a').click(function() {
 });
 });
 
-$('#slide ul').prepand($('#slide li:last-child'));
+$('#slide ul').prepend($('#slide li:last-child'));
+$('#slide ul').css('left', 'calc(-100vw + 30px)');
+var dir = -1;
+// スクロールのインターバル時間
+var interval = 3000;
+// スクロールのスピード
+var duration = 700;
+var timer;
+timer = setInterval(slideTimer, interval);
+function slideTimer() {
+    $('#slide ul').animate({'left':'-=calc(100vw - 30px)'}, duration,
+    function() {
+        $(this).append($('#slide li:first-child'));
+        $(this).css('left', 'calc(100vw - 30px)');
+    });
+}
+
+
